81efc0c01ca911b1e5e38b1da905619c253310fc
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-05-08 19:41:56 -0400

    add ajax

--- java.js
+++ java.js
@@ -1,2 +1,12 @@
 $(document).ready(function () {
-	
+
+	var title= "dogs";
+	var api= "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+
+	$.ajax({
+		url: api,
+		method: GET,
+	}).done(function(response) {
+		console.log(response);
+	});
+});
