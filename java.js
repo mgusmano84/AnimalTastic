69794ec3b41372df6e416c69c1325349750aa6d0
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-05-09 22:10:26 -0400

    added colored buttons

--- java.js
+++ java.js
@@ -1,38 +1,56 @@
+
 $(document).ready(function () {
 
 	var animals= ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
 	
+	//Create the buttons fron the animals array
+	function renderbuttons(){
+		$("#buttons").empty();
+		for (var i =0; i<animals.length; i++) {
+			var a = $('<button type="button" class="btn btn-primary buttonSpaces">');
+			a.addClass("animalClass");
+			a.attr('data-name', animals[i]);
+			a.text(animals[i]);
+			$('#buttons').append(a);
+		}
+	};
 
+	//Create the display of each animal
 	function displayAnimals(){
 		var api= "http://api.giphy.com/v1/gifs/search?q="+ animal +"&api_key=dc6zaTOxFJmzC";
-		var animal= %(this).attr('data-name');
+		var animal= $(this).attr('data-name');
 		
+		//ajax pulls the data from giphy
 		$.ajax({
 			url: api,
 			method: "GET",
 		}).done(function(response) {
-				console.log(response.data);
-				var results= response.data;
-					for (var i = 0; i < results.length; i++) {
-					var p = $('<p>').text("Rating: " + results[i].rating);	
-					var animalType=$('.items');
-					var animalImage= $('<img>');
-					animalImage.attr("src", results[i].images.fixed_width_small.url);
-					animalType.append(p);
-					animalType.append(animalImage);
-				};
+			console.log(response.data);
+			var results= response.data;
+			for (var i = 0; i < results.length; i++) {
+				var p = $('<p>').text("Rating: " + results[i].rating);	
+				var animalType=$('.items');
+				var animalImage= $('<img>');
+				animalImage.attr("src", results[i].images.fixed_width_small.url);
+				animalType.append(p);
+				animalType.append(animalImage);
+			};
 		});
 	};
-	displayAnimals();
+					
+	renderbuttons();
+	$(".animalClass").click(displayAnimals());
+	
+
 
 });
+
+
+
+
 
 // $(".animalType").click(function() {});
 
 
-// function renderbuttons(){
-// 				$("#buttons").empty();
-// 					for (var i =0; i<animals.length; i++) {
-// 						var a = $('<button>');
-// 						a.addClass("animal");
+
 			
